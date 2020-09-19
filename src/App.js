05f5f8a6944e991ede8add57f95bb4b990a7bed5
Ludvik <luddmachine@gmail.com>
2020-09-19 18:05:06 +0200

    quickfix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-
-import Button from 'react-bootstrap/Button';
+import React from 'react'
+import Button from 'react-bootstrap/Button'
 
 export default () => {
   return (
     <div>
       <p>Hei Verden</p>
-      <Button variant='success'>Kill self</Button>
+      <Button variant='alert'>Kill self</Button>
     </div>
-  );
-};
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'))
