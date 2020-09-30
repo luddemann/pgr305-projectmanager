8189dfe7edb873c19c9e33b3e6e9584f26218f22
Ludvik <luddmachine@gmail.com>
2020-10-01 01:58:16 +0200

    cleanup in projectlist, can now add project and soon edit it

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
@@ -7,28 +7,7 @@ import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import { useParams } from 'react-router-dom'
-
-const projects = [
-  {
-    id: 1,
-    title: 'Test title',
-    status: 'Under development',
-    assignedEmployees: [{ name: 'John' }, { name: 'Sarah' }],
-    client: 1,
-  },
-  {
-    id: 2,
-    title: 'Test title 2',
-    status: 'Completed',
-    assignedEmployees: [{ name: 'John' }],
-  },
-  {
-    id: 3,
-    title: 'Test title 3',
-    status: 'Not started',
-    assignedEmployees: [{ name: 'Sarah' }],
-  },
-]
+import { getRandomId } from '../utils/utils'
 
 const projectStatuses = [
   {
@@ -42,20 +21,36 @@ const projectStatuses = [
   },
 ]
 
-const employeesToAdd = []
-
 const ProjectList = () => {
   const { employees } = useEmployeeContext()
-  const [projectList, setProjectList] = useState(projects)
-  const [statuses] = useState(projectStatuses)
-  const [clickedItem, setClickedItem] = useState([])
+  const [clickedItem, setClickedItem] = useState()
   const [show, setShow] = useState(false)
   const [projectTitle, setProjectTitle] = useState('')
-  const [employeesOnProject, setEmployeesOnProject] = useState(employeesToAdd)
+  const [employeesOnProject, setEmployeesOnProject] = useState([])
+  const [editProject, setEditProject] = useState()
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      title: 'Test title',
+      status: 'Under development',
+      assignedEmployees: ['John', 'Sarah'],
+      client: 1,
+    },
+    {
+      id: 2,
+      title: 'Test title 2',
+      status: 'Completed',
+      assignedEmployees: ['John'],
+    },
+    {
+      id: 3,
+      title: 'Test title 3',
+      status: 'Not started',
+      assignedEmployees: ['Sarah'],
+    },
+  ])
 
-  const handleProjectTitle = (e) => {
-    setProjectTitle(e.target.value)
-  }
+  const handleProjectTitle = (e) => setProjectTitle(e.target.value)
 
   const handleClose = () => setShow(false)
   const handleOpen = (id) => {
@@ -64,70 +59,108 @@ const ProjectList = () => {
   }
 
   const handleEmployeesOnProject = (item) => {
-    if (employeesToAdd.indexOf(`${item}`) !== -1) {
-      alert('Jett')
-    } else setEmployeesOnProject(employeesToAdd.push(item))
+    if (employeesOnProject.find((e) => e === item)) {
+      alert(item + ' is already added to the project')
+    } else {
+      setEmployeesOnProject([...employeesOnProject, item])
+    }
   }
 
-  const deleteEmployeeFromProject = (item) => {
-    setEmployeesOnProject(employeesToAdd.splice(item))
+  const deleteEmployeeFromProject = (item) =>
+    setEmployeesOnProject([...employeesOnProject.filter((e) => e !== item)])
+
+  const handleSetClickedItem = (id) =>
+    setClickedItem(projects.find((project) => project.id === id))
+
+  const addProject = () => {
+    if (!projectTitle) {
+      alert('Mangler tittel')
+    } else {
+      setProjects([
+        ...projects,
+        {
+          id: getRandomId().toString().substring(0, 4),
+          title: projectTitle,
+          status: 'Not started',
+          assignedEmployees: employeesOnProject,
+        },
+      ])
+    }
   }
 
-  const handleSetClickedItem = (id) => {
-    setClickedItem(projectList.filter((project) => project.id === id))
+  useEffect(() => {
+    if (clickedItem) {
+      setEditProject(clickedItem)
+    }
+  }, [clickedItem])
+
+  const handleEdit = (e, prop) => {
+    console.log(projects) // får inn endrede elementer, trenger en funksjon for å sette editProject til project
+    setEditProject({
+      ...editProject,
+      [prop]: e.target.value,
+    })
   }
 
-  const renderedResult = clickedItem.map((item) => {
-    return (
-      <div key={item.id}>
-        <Modal.Header>
-          <Modal.Title style={{ display: 'flex', flexDirection: 'row' }}>
-            Project:
+  // funksjonen som skal bytte prosjektet med de redigerte elementene
+  /* const submit = (clickedItem.id) => {
+    const projectToEdit = projects.find((p) => p.id === clickedItem.id)
+
+    setProjects(...projects)
+  } */
+
+  // Kanskje bytte navn på denne
+  const renderedResult = clickedItem && (
+    <div key={clickedItem.id}>
+      <Modal.Header>
+        <Modal.Title style={{ display: 'flex', flexDirection: 'row' }}>
+          Project: {clickedItem.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId='exampleForm.ControlInput1'>
+            <Form.Label>Title</Form.Label>
             <Form.Control
               type='text'
-              defaultValue={item.title}
-              className='ml-2'
-            ></Form.Control>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId='exampleForm.ControlInput1'>
-              <Form.Label>Title</Form.Label>
-              <Form.Control type='text' defaultValue={item.title} />
-            </Form.Group>
-            <Form.Group controlId='exampleForm.ControlSelect1'>
-              <Form.Label>Status</Form.Label>
-              <Form.Control as='select'>
-                {statuses.map((status) => (
-                  <option
-                    key={status.title}
-                    defaultValue={status.title === item.status}
-                  >
-                    {status.title}
-                  </option>
-                ))}
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId='exampleForm.ControlSelect2'>
-              <Form.Label>Employees assigned</Form.Label>
-              <Form.Control as='select' multiple>
-                {employees.map((employee) => (
-                  <option key={employee.name}>{employee.name}</option>
-                ))}
-              </Form.Control>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant='primary'>Save Changes</Button>
-        </Modal.Footer>
-      </div>
-    )
-  })
+              defaultValue={clickedItem.title}
+              onChange={(e) => handleEdit(e, 'title')}
+              value={editProject?.title}
+            />
+          </Form.Group>
+          <Form.Group controlId='exampleForm.ControlSelect1'>
+            <Form.Label>Status</Form.Label>
+            <Form.Control
+              as='select'
+              defaultValue={clickedItem.status}
+              value={editProject?.status}
+              onChange={(e) => handleEdit(e, 'status')}
+            >
+              {projectStatuses.map((status) => (
+                <option key={status.title}>{status.title}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId='exampleForm.ControlSelect2'>
+            <Form.Label>Employees assigned</Form.Label>
+            <Form.Control as='select' multiple>
+              {clickedItem.assignedEmployees.map((employee) => (
+                <option key={employee}>{employee}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant='primary' type='submit'>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </div>
+  )
 
   return (
     <>
@@ -142,22 +175,30 @@ const ProjectList = () => {
           </tr>
         </thead>
         <tbody>
-          {projects.map((project) => (
-            <tr key={project.id}>
-              <td>{project.id}</td>
-              <td>{project.title}</td>
-              <td>{project.status}</td>
-              <td>{project.assignedEmployees.map((emp) => emp.name)}</td>
-              <td>
-                <Button
-                  variant='primary'
-                  onClick={() => handleOpen(project.id)}
-                >
-                  Edit
-                </Button>
-              </td>
-            </tr>
-          ))}
+          {projects.map((project) => {
+            return (
+              <tr key={project.id}>
+                <td>{project.id}</td>
+                <td>{project.title}</td>
+                <td>{project.status}</td>
+                <td>
+                  {project.assignedEmployees.map((employee, index) =>
+                    index === project.assignedEmployees.length - 1
+                      ? `${employee} `
+                      : `${employee}, `
+                  )}
+                </td>
+                <td>
+                  <Button
+                    variant='primary'
+                    onClick={() => handleOpen(project.id)}
+                  >
+                    Edit
+                  </Button>
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </Table>
 
@@ -189,36 +230,26 @@ const ProjectList = () => {
                   className='m-0'
                   onClick={() => handleEmployeesOnProject(employee.name)}
                 >
-                  +
+                  Add
                 </Button>
               </div>
             ))}
           </Form.Row>
           <p>Employees to be added:</p>
-          {employeesToAdd.map((employee) => (
-            <>
-              <div className='my-2' key={employee}>
-                <span style={{ fontWeight: 'bold' }}>{employee}</span>
-                <Button
-                  onClick={() =>
-                    deleteEmployeeFromProject(
-                      employeesToAdd.indexOf(employee.name)
-                    )
-                  }
-                  className='ml-1'
-                  variant='danger'
-                >
-                  x
-                </Button>
-              </div>
-            </>
+          {employeesOnProject?.map((employee) => (
+            <div className='my-2' key={employee}>
+              <span style={{ fontWeight: 'bold' }}>{employee}</span>
+              <Button
+                onClick={() => deleteEmployeeFromProject(employee)}
+                className='ml-1'
+                variant='danger'
+              >
+                x
+              </Button>
+            </div>
           ))}
         </Form.Group>
-        <Button
-          variant='primary'
-          className='mt-2'
-          onClick={() => console.log(employeesToAdd)}
-        >
+        <Button variant='primary' className='mt-2' onClick={addProject}>
           Add project
         </Button>
       </Form>
